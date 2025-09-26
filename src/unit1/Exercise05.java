587c94e46e937f779c69e2141064f8b2965308c0
salvador.pulido <salvador.pulido@106-23.info2>
2025-09-26 13:25:13 +0200

    Ejercicio 5 Circunferencia

---
+++ src/unit1/Exercise05.java
@@ -0,0 +1,37 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class exercise05 {
+
+	public static void main(String[] args) {
+		
+	    //radio de la circunferencia
+		double radio;
+		
+		//Longitud de la circunferencia
+		double longitud;
+		
+		//area de la circunferencia
+		double area;
+		
+		//creacion de la circunferencia
+		Scanner reader = new Scanner(System.in);
+		
+		//introduce el radio
+		System.out.println("Introduce el radio de la circunferencia");
+		radio = reader.nextDouble();// En este paso el usuario introduce el radio
+	
+		
+		//Calculamos la longitud de la circunferencia
+		longitud = 2*Math.PI*radio;
+		
+		//Calculamos el area de la circunferencia
+	    area = Math.PI*Math.pow(radio, 2);
+		
+		//Mostramos en pantalla el resultado de la longitud y el area
+		System.out.println("la longitud de la circunferencia es " + longitud);
+		System.out.println("el area de la circunferencia es " + area);
+		
+	}
+}
