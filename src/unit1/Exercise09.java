448250204ccca4d666c4d7151a909d90b3e58ed6
Salva <Salva@Salva-PC>
2025-09-29 00:46:40 +0200

    Pedir al usuario su edad y que le indique si es mayor de edad

--- src/unit1/Exercise09.java
+++ src/unit1/Exercise09.java
@@ -1,5 +1,14 @@
 package unit1;
 
 public class Exercise09 {
+	
+	import java.util.Scanner;
 
-}
+	Scanner scanner = new Scanner(System.in);
+	System.out.print("Introduce tu edad: ");
+	int edad = scanner.nextInt();
+	boolean mayorEdad = edad >= 18;
+	System.out.println("¿Eres mayor de edad?: " + mayorEdad);
+	scanner.close();
+	    }
+	}
