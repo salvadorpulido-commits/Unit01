d6ffcd1f3dd08c5676fe24c67d1c6eaf8897fa1d
Salva <Salva@Salva-PC>
2025-09-29 00:49:28 +0200

    conversor de pesetas a euros

--- src/unit1/Exercise11.java
+++ src/unit1/Exercise11.java
@@ -5,6 +5,13 @@ public class Exercise11 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		import java.util.Scanner;
 
-}
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Introduce la cantidad en pesetas: ");
+		double pesetas = scanner.nextDouble();
+		double euros = pesetas / 166.0;
+        System.out.println("Equivale a " + euros + " euros.");
+		scanner.close();
+		    }
+		}
