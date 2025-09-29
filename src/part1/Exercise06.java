59067c46bc08eb2b3d1b0e42fcb51019d9eed57a
Salva <Salva@Salva-PC>
2025-09-29 21:32:44 +0200

    Comentarios añadidos

--- src/part1/Exercise06.java
+++ src/part1/Exercise06.java
@@ -3,16 +3,31 @@ package part1;
 import java.util.Scanner;
 
 public class Exercise06 {
+	
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    	/*Escribir un programa que le pida dos números al usuario. 
+    	 * A continuación, debe mostrar la suma, la resta, la multiplicación y la división de ambos números. 
+    	 * Debe mostrarse el resultado de cada operación en una línea distinta.
+    	*/
+    	
+        Scanner sc = new Scanner(System.in);
+        
+        //Pedimos el primer numero al usuario
         System.out.print("Introduce el primer número: ");
-        double num1 = scanner.nextDouble();
+        double num1 = sc.nextDouble();
+        
+        //Pedimos el segundo numero al usuario
         System.out.print("Introduce el segundo número: ");
-        double num2 = scanner.nextDouble();
+        double num2 = sc.nextDouble();
+        
+        //Hacemos las operaciones cada una en una linea distinta
+        
         System.out.println("Suma: " + (num1 + num2));
         System.out.println("Resta: " + (num1 - num2));
         System.out.println("Multiplicación: " + (num1 * num2));
         System.out.println("División: " + (num1 / num2));
-        scanner.close();
+        
+        //Cerramos Scanner
+        sc.close();
     }
 }
