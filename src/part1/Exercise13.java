980afb627840b34838b85ab6030483b2b43041b2
Salva <Salva@Salva-PC>
2025-09-29 23:13:07 +0200

    comentarios añadidos

--- src/part1/Exercise13.java
+++ src/part1/Exercise13.java
@@ -7,34 +7,44 @@ public class Exercise13 {
 	public static void main(String[] args) {
 		
 		/*
-		 * Pedir al usuario medianye booleno si llueve, si ha terminado las tareas y
-		 * tiene que ir a la biblio
-		 * mostrar mesaje que infdique si puede salir
+		 * Pedir al usuario mediante booleno si llueve, si ha terminado las tareas y
+		 * si tiene que ir a la biblioteca
+		 * mostrar mesaje que identifique si puede salir
 		 */
-		
+		//Creamos scanner
 		Scanner reader = new Scanner(System.in);
 
-        //Variable de 
+        //Variable lluvia, tareas y biblioteca
 
         boolean estaLloviendo;
         boolean tareasTerminadas;
         boolean irBiblioteca;
         
-        System.out.println("¿Esta lloviendo?");
-        estaLloviendo = reader.nextBoolean();
-        
-        System.out.println("¿Hasteminado las tareas?");
-        tareasTerminadas = reader.nextBoolean();
-        
-        System.out.println("¿Tienes que ir a la biblioteca?");
-        irBiblioteca = reader.nextBoolean();
-        
-        //Si no esta lloviendo y hemos finalizado nuestras tareas. Indistintamente 
-        //de lo anterior, podemos salir a la calle. El hecho de tener que ir a la biblioteca
-        salir = (!estaLloviendo && tareasTerminadas) ||
-        		
-        	
+        // Variable para saber si puedo salir a la calle
+        boolean salir;
 
-	}
+        // Preguntamos al usuario   
+     	System.out.println("¿Está lloviendo?");
+     	estaLloviendo = reader.nextBoolean();
+
+     	System.out.println("¿Has terminado las tareas?");
+     	tareasTerminadas = reader.nextBoolean();
+
+     	System.out.println("¿Tienes que ir a la biblioteca?");
+     	irBiblioteca = reader.nextBoolean();
+
+     	/* si no está lloviendo y hemos finalizado nuestras tareas. indistintamente de
+     	/ lo anterior, podremos salir a la calle: el hecho de tener que ir a la
+     	*/ biblioteca.
+     	
+     	
+     	salir = (!estaLloviendo && tareasTerminadas) || irBiblioteca;
+     	
+     	//Respondemos     		
+     	System.out.println("¿Puedo salir a la calle? " + salir );
+     	
+        //cerramos scanner
+     	reader.close();
+     	}
 
 }
