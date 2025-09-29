63adda6cc9b498726ca0ae995a6de843e5e5bd25
Salva <Salva@Salva-PC>
2025-09-29 22:14:37 +0200

    comentarios y correccionn de errores añadidos

--- src/part1/Exercise10.java
+++ src/part1/Exercise10.java
@@ -7,30 +7,30 @@ public class Exercise10 {
 	public static void main(String[] args) {
 		/*
 		 * 1 Pedir un numero al usuario
-		 * compuebo si es par
-		 * imprimo el mensaje
+		 * 2 compuebo si es par
+		 * 3 imprimo el mensaje
 		 */
 		
 		// Creamos el scanner
 		Scanner reader = new Scanner(System.in);
+		
+		//Declaramos variables
 		int numero;
 		boolean esPar;
+		String cadenaEsPar;
 		
 		// Pedimos un numero al usuario
-		System.out.println("introduzca su numero");
+		System.out.println("introduzca su numero:");
 		numero = reader.nextInt();
 		
 		// Comprobamos si el numero es par
-		esPar = numero%2 == 0;
+		esPar = (numero%2 == 0) ? "Si" : "No";
 		
-		// Imprimimos si el nuemro es par o impar
-		System.out.println("El numero es par" + esPar);
+		// Imprimimos si el numero es par o impar
+		System.out.println("¿El numero es par?" + cadenaEsPar);
 		
 	    // Cerramos el scanner
 		reader.close();
-		
-		
-		
-				
+								
 	}
 }
