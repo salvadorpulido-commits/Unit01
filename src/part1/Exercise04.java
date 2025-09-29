2e2713fca6973ff4a68487082b3056d549e504da
Salva <Salva@Salva-PC>
2025-09-29 21:04:32 +0200

    Comentarios añadidos

--- src/part1/Exercise04.java
+++ src/part1/Exercise04.java
@@ -3,14 +3,28 @@ package part1;
 import java.util.Scanner;
 
 public class Exercise04 {
+	
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    	
+    	// Creamos nuevo  scanner
+        Scanner sc = new Scanner(System.in);
+        
+        // Pedimos la primera nota al usuario
         System.out.print("Introduce la primera nota: ");
-        int nota1 = scanner.nextInt();
+        int nota1 = sc.nextInt();
+        
+        // Pedimos la segunda nota al usuario
         System.out.print("Introduce la segunda nota: ");
-        int nota2 = scanner.nextInt();
+        int nota2 = sc.nextInt();
+        
+        // Calculamos la media de las dos notas, indicando que puede tener decimales
         double media = (nota1 + nota2) / 2.0;
+        
+        // Imprimimos resultado
         System.out.println("La media es: " + media);
+        
+        // Cerramos Scanner
         scanner.close();
     }
+    
 }
