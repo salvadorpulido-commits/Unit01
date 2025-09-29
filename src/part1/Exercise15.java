b5ba49ead78faa36a206f9d9fdc3156dfb6e248c
Salva <Salva@Salva-PC>
2025-09-29 23:26:22 +0200

    comentarios añadidos

--- src/part1/Exercise15.java
+++ src/part1/Exercise15.java
@@ -10,26 +10,29 @@ public class Exercise15 {
 		 * Pedir precio al usuario
 		 * mostrar el precio con el iva
 		 */
-		// Creamos Scanner
-		Scanner reader = new Scanner(System.in);
-		
-		//Constante almacenar el valor del IVA
-		final int IVA = 21;
-		
-		//precio para leer la consola
-		double precio;
-		//variale donde almacenar el precio total con el IVA
-		
-		double precioConIVA;
-		
-		System.out.println("Introduce el precio:");
-		precio = reader.nextDouble();
-		
-		precioConIVA = precio+precio*IVA/100;
-		System.out.println("El precio con IVA es: " + precioConIVA);
-		
-	    reader.close();
-		
-	}
+		// Creamos el Scanner
+				Scanner reader = new Scanner(System.in);
 
-}
+		// Constante para almacenar el valor del IVA
+		final int IVA = 21;
+
+		// Precio a leer de consola
+		double precio;
+
+		// Variable donde almacenar el precio total con el IVA incluido
+		double precioConIVA;
+
+		// Le pedimos el precio al usuario
+		System.out.println("Introduzca el precio:");
+		precio = reader.nextDouble();
+
+		// Calculamos el precion con IVA incluido
+		precioConIVA = precio + precio * IVA / 100;
+		System.out.println("El precio con IVA es: " + precioConIVA);
+
+		// Cerramos Scanner
+		reader.close();
+
+			}
+
+		}
