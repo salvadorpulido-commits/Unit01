d012d7519ec423b134f355f8278ff1a557a1a36f
Salva <Salva@Salva-PC>
2025-09-29 23:28:46 +0200

    correcciones

--- src/part2/Exercise01.java
+++ src/part2/Exercise01.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 public class Exercise01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-        /**
+        /*
          * Realizar un programa que pida como entrada un número con decimales
          * y lo muestre redondeado al entero más próximo.
          * SIN UTILIZAR Math.round
