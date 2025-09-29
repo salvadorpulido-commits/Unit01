9a1dcfc953476c9269e075bb7edb3d264d9598e0
Salva <Salva@Salva-PC>
2025-09-29 22:46:23 +0200

    comentarios y correcciones añadidos

--- src/part1/Exercise11.java
+++ src/part1/Exercise11.java
@@ -5,13 +5,31 @@ import java.util.Scanner;
 public class Exercise11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * /*Realiza un conversor de pesetas a euros.
+		 * pide al usuario que te introduzca el valor en pesetas 
+		 * y muestrale el resultado de la conversion
+		 * (1€ = 166 ptas).
+		 */
 
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Introduce la cantidad en pesetas: ");
-		double pesetas = scanner.nextDouble();
+		//Creamos scanner
+		Scanner sc = new Scanner(System.in);
+		
+				
+		//Declaramos variable pesetas
+		double pesetas = sc.nextDouble();
+		
+		//Declaramos variable euros
 		double euros = pesetas / 166.0;
+		
+		//Pedimos al usuario introducir la cantidad a convertir
+		System.out.print("Introduce la cantidad en pesetas: ");
+		pesetas = sc.nextDouble();
+		
+		//Devolvemos resultado
 		System.out.println("Equivale a " + euros + " euros.");
-		scanner.close();
+		
+		//Cerramos scanner
+		sc.close();
 	}
 }
