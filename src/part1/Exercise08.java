4a6dfc822d1887d557ed2861e051615198d55ebd
Salva <Salva@Salva-PC>
2025-09-29 21:52:46 +0200

    comentarios añadidos

--- src/part1/Exercise08.java
+++ src/part1/Exercise08.java
@@ -13,11 +13,23 @@ public class Exercise08 {
 		// Creamos nuevo Scanner
 		Scanner reader = new Scanner(System.in);
 
-		System.out.print("Introduce tu nombre: ");
+		//Declaramos variable nombre
 		String nombre = reader.nextLine();
-		System.out.print("Introduce tu edad: ");
+		
+		//Declaramos variable edad
 		int edad = reader.nextInt();
+		
+		//Pedimos nombre al usuario
+		System.out.print("Introduce tu nombre: ");
+		
+		//Pedimos edad al usuario
+		System.out.print("Introduce tu edad: ");
+		
+		//Mostramos mensaje
 		System.out.println("Hola " + nombre + ", tienes " + edad + " años, ¡qué mayor eres!");
+		
+		//Cerramos scanner
 		reader.close();
+		
 	}
 }
