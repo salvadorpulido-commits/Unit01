d8324b9e24e2d021b1ed8308408fe617ef35638a
Salva <Salva@Salva-PC>
2025-09-29 22:07:17 +0200

    comentarios añadidos y correccion de errores

--- src/part1/Exercise09.java
+++ src/part1/Exercise09.java
@@ -5,11 +5,31 @@ import java.util.Scanner;
 public class Exercise09 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Introduce tu edad: ");
-		int edad = scanner.nextInt();
+		
+		/*
+		 * Realizar una aplicación que solicite al usuario su edad 
+		 * y le indique si es mayor de edad
+		 * (mediante un literal booleano: true o false).
+		 */
+		
+		//Creamos nuevo scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//Declaramos variable edad
+		int edad = sc.nextInt();
+		
+		//Declaramos variable boolean
 		boolean mayorEdad = edad >= 18;
+		
+		//Pedimos edad al usuario
+		System.out.print("Introduce tu edad: ");
+		edad = sc.nextInt();
+		
+		//Mostramos si la persona es mayor de edad
 		System.out.println("¿Eres mayor de edad?: " + mayorEdad);
-		scanner.close();
+		
+		
+		//Cerramos scanner
+		sc.close();
 	}
 }
