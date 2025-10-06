b1caeeae541706dbe024e7c996b9ed7ab2220b0f
Salva <Salva@Salva-PC>
2025-10-06 20:30:15 +0200

    comentarios y orden

--- src/englishExercisesPart02/Exercise06.java
+++ src/englishExercisesPart02/Exercise06.java
@@ -1,33 +1,64 @@
 package englishExercisesPart02;
 
-import java.util.Scanner;   // Importamos la clase Scanner para leer datos del usuario
+import java.util.Scanner;
 
 public class Exercise06 {
 
     public static void main(String[] args) {
+    	
+    	/*
+    	 * A group of students has been told to get into teams of a specific size 
+    	 * for their coursework. 
+    	 * Design and implement a program that prompts for the number of students 
+    	 * in the group and the size of the teams to be formed, 
+    	 * and displays how many teams can be formed and how many students 
+    	 * are left without a team.
+    	 */
+
         
-        // Creamos un objeto Scanner llamado 'sc' para leer los datos del teclado
+        // Creamos Scanner
         Scanner sc = new Scanner(System.in);
         
+        //Variable total de estudiantes
+        
+        int totalStudents
+        
+        //Variable tamaño de cada equipo
+        
+        int teamSize
+        
+        //Variable equipos completos
+        
+        int fullTeams
+        
+        //Variable estudiantes sin equipo
+        
+        int studentsLeft
+                       
         // Pedimos al usuario que introduzca el número total de estudiantes
+        
         System.out.print("Enter the total number of students: ");
-        int totalStudents = sc.nextInt();   // Leemos el número total de estudiantes
+        int totalStudents = sc.nextInt();
         
         // Pedimos al usuario que introduzca el tamaño de cada equipo
+        
         System.out.print("Enter the desired team size: ");
-        int teamSize = sc.nextInt();   // Leemos el tamaño de cada equipo
+        int teamSize = sc.nextInt(); 
         
         // Calculamos cuántos equipos completos se pueden formar
+        
         int fullTeams = totalStudents / teamSize;
         
         // Calculamos cuántos estudiantes quedan sin equipo (resto de la división)
+        
         int studentsLeft = totalStudents % teamSize;
         
         // Mostramos los resultados al usuario
+        
         System.out.println("Number of full teams that can be formed: " + fullTeams);
         System.out.println("Number of students without a team: " + studentsLeft);
         
-        // Cerramos el Scanner para liberar recursos del sistema
+        // Cerramos Scanner
         sc.close();
     }
 }
