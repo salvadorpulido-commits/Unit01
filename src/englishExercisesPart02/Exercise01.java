aec7607c245c5803ffe97f6efe6c61dc2d1085c6
Salva <Salva@Salva-PC>
2025-10-06 18:33:46 +0200

    Exercise01

---
+++ src/englishExercisesPart02/Exercise01.java
@@ -0,0 +1,62 @@
+package englishExercisesPart02;
+
+import java.util.Scanner;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Implement in Java the next program:
+		 * Begin
+		 * Display program title
+		 * Display prompt for price
+		 * Enter price
+		 * Display prompt for tax
+		 * Enter tax
+		 * Set price to price * (1 +tax/100)
+		 * Display new price
+		 * End
+		 */
+
+        //Creammos Scanner
+		
+		Scanner sc = new Scanner(system.in);
+		
+		// Variable precio
+		
+		double price;
+		
+		//Variable impuesto
+		
+		int tax;
+		
+		//Nombramos programa
+		
+		System.out.println("TAX CALCULATOR");
+		
+		//Solicitamos importe
+		
+		System.out.println("Introduce the price");
+		price = sc.nextDouble();
+		
+		//Solicitamos impuesto
+		
+		System.out.println("Introduce tax");
+		tax = sc.nextInt;
+		
+		//Calcula el resultado
+		
+		price = price *(1 + tax/100);
+		
+		//Muestra el resultado
+		
+		System.out.println("The price with the tax is " + price);
+		
+		//Cerramos Scanner
+		
+		sc.close();
+		
+	}
+
+}
