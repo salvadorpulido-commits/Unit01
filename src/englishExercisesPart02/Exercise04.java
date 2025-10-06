f5452ded9e7c99b3699a6697b4ab89e986091638
Salva <Salva@Salva-PC>
2025-10-06 20:18:10 +0200

    comentarios y correcciones

--- src/englishExercisesPart02/Exercise04.java
+++ src/englishExercisesPart02/Exercise04.java
@@ -1,17 +1,37 @@
 package englishExercisesPart02;
 
-import java.util.Scanner;   // Importamos la clase Scanner para leer datos del usuario
+import java.util.Scanner;   
 
 public class Exercise04 {
 
     public static void main(String[] args) {
         
-        // Creamos un objeto Scanner llamado 'sc' para leer datos del teclado
+    	/*
+    	 * The following pseudocode has been arrived at for a program that converts 
+    	 * pounds to kilos (1 kilo = 2.2 lb):
+    	 * BEGIN
+    	 * PROMPT for value in pounds
+    	 * ENTER value in pounds
+    	 * SET value to old value ÷ 2.2
+    	 * DISPLAY value in kilos
+    	 * END
+    	 * Implement this program, remembering to declare any variables that are necessary.
+    	 */
+    	
+        // Creamos Scanner
         Scanner sc = new Scanner(System.in);
+        
+        //Declaramos variable Libras
+        
+        double pounds
+        
+        //Declaramos variable Kilogramos
+        
+        double kilos
         
         // Pedimos al usuario que introduzca el valor en libras
         System.out.print("Enter the value in pounds: ");
-        double pounds = sc.nextDouble();   // Guardamos el valor en una variable de tipo double
+        double pounds = sc.nextDouble(); 
         
         // Convertimos libras a kilos dividiendo entre 2.2 (ya que 1 kilo = 2.2 libras)
         double kilos = pounds / 2.2;
@@ -19,7 +39,7 @@ public class Exercise04 {
         // Mostramos el resultado en pantalla
         System.out.println(pounds + " pounds are equivalent to " + kilos + " kilos.");
         
-        // Cerramos el Scanner para liberar los recursos del sistema
+        // Cerramos Scanner
         sc.close();
     }
 }
