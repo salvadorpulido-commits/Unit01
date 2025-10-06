4ff7fd61837daff72606bb22dd0c6940b998dffc
Salva <Salva@Salva-PC>
2025-10-06 20:24:24 +0200

    orden de lineas y comentarios

--- src/englishExercisesPart02/Exercise05.java
+++ src/englishExercisesPart02/Exercise05.java
@@ -1,21 +1,38 @@
 package englishExercisesPart02;
 
-import java.util.Scanner;   // Importamos la clase Scanner para leer datos del usuario
+import java.util.Scanner;   
 
 public class Exercise05 {
 
     public static void main(String[] args) {
+    	
+    	/*
+    	 * An individual’s Body Mass Index (BMI) is a measure of a person’s weight 
+    	 * in relation to their height. It is calculated as follows:
+    	 * Divide a person’s weight (in kg) by the square of their height (in meters)
+    	 */
         
-        // Creamos un objeto Scanner llamado 'sc' para leer los datos introducidos por el usuario
+        // Creamos Scanner
         Scanner sc = new Scanner(System.in);
+        
+        //Declaramos variable Peso
+        double weight
+        
+        //Declaramos variable Altura
+        
+        double height
+        
+        //Declaramos variable Indice masa corporal
+        
+        double bmi
         
         // Pedimos al usuario que introduzca su peso en kilogramos
         System.out.print("Enter your weight in kilograms: ");
-        double weight = sc.nextDouble();   // Guardamos el peso como número decimal (double)
+        double weight = sc.nextDouble();  
         
         // Pedimos al usuario que introduzca su altura en metros
         System.out.print("Enter your height in meters: ");
-        double height = sc.nextDouble();   // Guardamos la altura como número decimal (double)
+        double height = sc.nextDouble(); 
         
         // Calculamos el BMI usando la fórmula: peso / (altura al cuadrado)
         double bmi = weight / (height * height);
@@ -23,7 +40,7 @@ public class Exercise05 {
         // Mostramos el resultado al usuario
         System.out.println("Your Body Mass Index (BMI) is: " + bmi);
         
-        // Cerramos el Scanner para liberar recursos del sistema
+        // Cerramos Scanner
         sc.close();
     }
 }
